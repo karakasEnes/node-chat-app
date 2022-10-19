3f7a250aded1da37eed8e9a81cd8b87f2b6aceb9
Enes Karakas <enes.coding@gmail.com>
2022-10-19 22:33:39 +0300

    web socket connection done

--- main/src/app.ts
+++ main/src/app.ts
@@ -6,11 +6,14 @@ import express from 'express';
 import { Request, Response } from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import http from 'http';
+import * as socketio from 'socket.io';
 //file imports
 
 //express Setup
 const app: express.Application = express();
-
+const server = http.createServer(app);
+const io = new socketio.Server(server);
 //PORT
 const STATIC_PORT = 4343;
 const PORT = process.env.PORT || STATIC_PORT;
@@ -22,11 +25,11 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-//routes!
-app.get('/', (_req, _res) => {
-  _res.render('index');
+//io
+io.on('connection', (_socket) => {
+  console.log('New Web Socket connection!');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('listenin on port ' + PORT);
 });
