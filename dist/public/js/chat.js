ac671d01074304d39be4c1938647148ec98ee134
Enes Karakas <enes.coding@gmail.com>
2022-10-20 19:57:39 +0300

    adding messages to the UI, without using mustaches

--- dist/public/js/chat.js
+++ dist/public/js/chat.js
@@ -1,30 +1,46 @@
 "use strict";
 // import { io } from 'socket.io-client';
 const socket = io();
-socket.on('message', (m) => {
-    console.log(m);
-});
 const form = document.querySelector('form');
+const btnForm = document.querySelector('.btn-form');
 const inputEl = document.querySelector('.input-form');
 const btnLocation = document.querySelector('.location');
-btnLocation.addEventListener('click', () => {
-    if (!window.navigator) {
-        alert("Your browser doesn't support for geo location, please update it!");
-    }
-    window.navigator.geolocation.getCurrentPosition((pos) => {
-        const urlLoc = `https://google.com/maps?q=${pos.coords.latitude},${pos.coords.longitude}`;
-        socket.emit('sendLocation', urlLoc, (m) => {
-            console.log(m);
-        });
-    });
+const $messages = document.querySelector('.messages');
+const $templateHTML = document.querySelector('#message-template').innerHTML;
+socket.on('message', (m) => {
+    console.log(m);
+    //update template
+    const htmlMarkup = `
+    <div>
+      ${m}
+    </div>
+    `;
+    $messages.insertAdjacentHTML('beforeend', htmlMarkup);
 });
-form.addEventListener('submit', (e) => {
+btnForm.addEventListener('click', (e) => {
     e.preventDefault();
+    btnForm.setAttribute('disabled', 'disabled');
     const value = inputEl.value;
     socket.emit('sendMessage', value, (err, m) => {
+        btnForm.removeAttribute('disabled');
+        inputEl.value = '';
+        inputEl.focus();
         if (err) {
             return console.log(err);
         }
         console.log(m);
+    });
+});
+btnLocation.addEventListener('click', () => {
+    if (!window.navigator) {
+        return alert("Your browser doesn't support for geo location, please update it!");
+    }
+    btnLocation.setAttribute('disabled', 'disabled');
+    window.navigator.geolocation.getCurrentPosition((pos) => {
+        const urlLoc = `https://google.com/maps?q=${pos.coords.latitude},${pos.coords.longitude}`;
+        socket.emit('sendLocation', urlLoc, (m) => {
+            console.log(m);
+            btnLocation.removeAttribute('disabled');
+        });
     });
 });
--- main/public/js/chat.ts
+++ main/public/js/chat.ts
@@ -1,37 +1,59 @@
 // import { io } from 'socket.io-client';
-
 const socket = io();
+
+const form = document.querySelector('form') as HTMLFormElement;
+const btnForm = document.querySelector('.btn-form') as HTMLButtonElement;
+const inputEl = document.querySelector('.input-form') as HTMLInputElement;
+const btnLocation = document.querySelector('.location') as HTMLButtonElement;
+const $messages = document.querySelector('.messages') as HTMLDivElement;
+const $templateHTML = document.querySelector('#message-template')!.innerHTML;
 
 socket.on('message', (m) => {
   console.log(m);
+  //update template
+  const htmlMarkup = `
+    <div>
+      ${m}
+    </div>
+    `;
+
+  $messages.insertAdjacentHTML('beforeend', htmlMarkup);
 });
 
-const form = document.querySelector('form') as HTMLFormElement;
-const inputEl = document.querySelector('.input-form') as HTMLInputElement;
-const btnLocation = document.querySelector('.location') as HTMLButtonElement;
+btnForm.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  btnForm.setAttribute('disabled', 'disabled');
+
+  const value = inputEl.value;
+  socket.emit('sendMessage', value, (err, m) => {
+    btnForm.removeAttribute('disabled');
+    inputEl.value = '';
+    inputEl.focus();
+
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log(m);
+  });
+});
 
 btnLocation.addEventListener('click', () => {
   if (!window.navigator) {
-    alert("Your browser doesn't support for geo location, please update it!");
+    return alert(
+      "Your browser doesn't support for geo location, please update it!"
+    );
   }
+
+  btnLocation.setAttribute('disabled', 'disabled');
 
   window.navigator.geolocation.getCurrentPosition((pos) => {
     const urlLoc = `https://google.com/maps?q=${pos.coords.latitude},${pos.coords.longitude}`;
 
     socket.emit('sendLocation', urlLoc, (m) => {
       console.log(m);
+      btnLocation.removeAttribute('disabled');
     });
-  });
-});
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const value = inputEl.value;
-  socket.emit('sendMessage', value, (err, m) => {
-    if (err) {
-      return console.log(err);
-    }
-
-    console.log(m);
   });
 });
